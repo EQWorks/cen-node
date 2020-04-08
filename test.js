45c10ba9c1091c9d1e2279e8559a3e45fb6f4c90
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2020-04-07 21:25:18 -0400

    utils - add report encode/decode/unfold functions

--- test.js
+++ test.js
@@ -1,19 +1,26 @@
 const assert = require('assert')
 
-const cen = require('.')
+const { utils } = require('.')
 
 
-const keyStr = cen.makeCENKeyString()
+const keyStr = utils.makeCENKeyString()
 assert.equal(keyStr.length, 32)
 
+const reportRaw = 'severe fever,coughing,hard to breathe,头疼'
+const cenKeysArr = [...new Array(2)].map(() => utils.makeCENKeyString())
 const payload = {
-  report: Buffer.from('severe fever,coughing,hard to breathe').toString('base64'),
-  cenKeys: [...new Array(2)].map(() => cen.makeCENKeyString()).join(','),
+  report: utils.encodeReport(reportRaw),
+  cenKeys: cenKeysArr.join(','),
   reportTimeStamp: 1586297540,
 }
+assert.equal(utils.decodeReport(payload.report), reportRaw)
 // payload in Object form
-const reportID = cen.computeHash(payload)
+const reportID = utils.computeHash(payload)
 assert.equal(reportID.length, 64)
 // behavior: payload in JSON string form should have consistent
 // hash generated from Object payload input ^
-assert.equal(cen.computeHash(JSON.stringify(payload)), reportID)
+assert.equal(utils.computeHash(JSON.stringify(payload)), reportID)
+
+const unfolded = utils.unfoldReport(payload)
+assert.equal(unfolded.report, reportRaw)
+assert.deepEqual(unfolded.cenKeys, cenKeysArr)
